dd671f14573016681c2b8cd18e13873a1bba4b1d
Stanislav Hlova <stanislav_hlova@epam.com>
2023-03-18 11:52:16 +0200

    dto: implementing POST requests for register, sending measurements.
    Implementing registration request of a sensor.
    Implementing sending the measurement to the server from client.

--- client/src/main/java/org/stanislav/dto/MeasurementDTO.java
+++ client/src/main/java/org/stanislav/dto/MeasurementDTO.java
@@ -1,6 +1,7 @@
 package org.stanislav.dto;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.LocalDateTime;
 
@@ -11,11 +12,18 @@ public class MeasurementDTO {
     private double temperature;
     private boolean raining;
     @JsonAlias(value = "sensor")
+    @JsonProperty(value = "sensor")
     private SensorDTO sensorDTO;
     @JsonAlias(value = "time")
     private LocalDateTime time;
 
     public MeasurementDTO() {
+    }
+
+    public MeasurementDTO(double temperature, boolean raining, SensorDTO sensorDTO) {
+        this.temperature = temperature;
+        this.raining = raining;
+        this.sensorDTO = sensorDTO;
     }
 
     public double getTemperature() {
